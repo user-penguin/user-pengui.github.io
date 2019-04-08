16347d1c905872c95613a613095f5b6077b3475e
user-penguin <kobzev96@gmail.com>
2019-04-09 01:20:46 +0700

    add mg

---
+++ _includes/javascript/site.js
@@ -0,0 +1,16 @@
+{% include /javascript/menu.js %}
+{% include /javascript/headspace.js %}
+{% include /javascript/currentyear.js %}
+
+$(function () {
+  Headspace(document.querySelector('[data-header]'), {
+    startOffset: 90,
+    tolerance: 5,
+    showAtBottom: false,
+    classNames: {
+      base: 'sticky-header',
+      fixed: 'sticky-header--fixed',
+      hidden: 'sticky-header--hidden'
+    }
+  })
+}());
